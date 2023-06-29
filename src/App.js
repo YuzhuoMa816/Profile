44bc0eab455e472281590b5dc6e28c81dcb8bf7a
YuzhuoMa816 <71178309+YuzhuoMa816@users.noreply.github.com>
2023-06-30 00:06:40 +1000

    First push, with init files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+
+const name = 'Yuzhuo Ma'
+const getAge = () =>{
+    return 18
+}
+
+const songs = [
+    {id:1, name:'痴心妄想'},
+    {id:2, name:'南山南'},
+    {id:3, name:'像我这样的人'},
+]
+const flag = false
+
+const getHtag = (type)=>{
+    if (type===1){
+        return <h1>this is h1</h1>
+    }
+    if(type===2){
+        return <h2>this is h2</h2>
+    }
+    if(type===3){
+        return  <h3>this is h3</h3>
+    }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+            {name}
+            {getAge() }
+          <u1>
+              {songs.map(song=><li key={song.id}> {song.name} </li>)}
+          </u1>
+          {flag ? (<div>
+                        <span> this is span </span>
+                    </div>): null}
+      </div>
+  )
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+
+//React kernel package
 import React from 'react';
+// ReactDOM render package
 import ReactDOM from 'react-dom/client';
+
+//全局样式文件
 import './index.css';
+//根组件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
